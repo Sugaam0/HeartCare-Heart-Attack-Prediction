c999e27b0cc2ade2db8cf90405b41052a4639c11
Sugam Acharya <108186879+Sugaam0@users.noreply.github.com>
2025-06-19 18:30:29 +0545

    Update settings.py

--- heart_attack_predictor/heart_attack_predictor/settings.py
+++ heart_attack_predictor/heart_attack_predictor/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 from decouple import config
@@ -77,12 +77,10 @@ WSGI_APPLICATION = 'heart_attack_predictor.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
