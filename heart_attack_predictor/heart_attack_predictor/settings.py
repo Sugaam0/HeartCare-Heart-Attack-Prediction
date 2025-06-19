5e7c43a7ec16da93870ce727b52fc46dde5ef467
Sugaam0 <sugamacharya091@gmail.com>
2025-06-19 11:16:05 +0545

    fx

--- heart_attack_predictor/heart_attack_predictor/settings.py
+++ heart_attack_predictor/heart_attack_predictor/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 
 # Quick-start development settings - unsuitable for production
@@ -22,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
