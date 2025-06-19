0417d6852fda4d8a67def23a1d481106fcc3c32a
Sugam Acharya <108186879+Sugaam0@users.noreply.github.com>
2025-06-19 17:57:17 +0545

    Update settings.py

--- heart_attack_predictor/heart_attack_predictor/settings.py
+++ heart_attack_predictor/heart_attack_predictor/settings.py
@@ -23,9 +23,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://heart-attack-predictor.up.railway.app/']
 
 
 
